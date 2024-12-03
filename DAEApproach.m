82fbefe84ad892af9737e6b29ee147be44b8b176
Mark Lohatepanont <mlohate1@swarthmore.edu>
2024-12-02 20:47:36 -0500

    Working_DAE_For_Y=X_Func

--- DAEApproach.m
+++ DAEApproach.m
@@ -4,7 +4,7 @@ syms x1 y1 theta1 x2 y2 theta2 x3 y3 theta3 x4 y4 real
 syms x1_dot y1_dot theta1_dot x2_dot y2_dot theta2_dot x3_dot y3_dot theta3_dot x4_dot y4_dot real
 syms theta1_ddot x2_ddot y2_ddot theta2_ddot x3_ddot y3_ddot theta3_ddot x4_ddot y4_ddot real
 syms rx1 ry1 rx2 ry2 rx3 ry3 d1 d2 d3 g m1 m2 m3 m4 ig1 ig2 ig3 real
-syms F x1_ddot y1_ddot real
+syms F F_N_x F_N_y x1_ddot y1_ddot real
 
 %Constraints
 c1 = x1_ddot - d1*sin(theta1)*theta1_dot^2 + d1*cos(theta1)*theta1_ddot == x2_ddot; %x1 
@@ -13,11 +13,13 @@ c3 = x1_ddot - d1*sin(theta1)*theta1_dot^2 - d2*sin(theta2)*theta2_dot^2 + d1*co
 c4 = d1*cos(theta1)*theta1_dot^2 + d2*cos(theta2)*theta2_dot^2 + y1_ddot + d1*sin(theta1)*theta1_ddot + d2*sin(theta2)*theta2_ddot == y3_ddot;
 c5 = x1_ddot - d1*sin(theta1)*theta1_dot^2 - d2*sin(theta2)*theta2_dot^2 - d3*sin(theta3)*theta3_dot^2 + d1*cos(theta1)*theta1_ddot + d2*cos(theta2)*theta2_ddot + d3*cos(theta3)*theta3_ddot == x4_ddot;
 c6 = d1*cos(theta1)*theta1_dot^2 + d2*cos(theta2)*theta2_dot^2 + d3*cos(theta3)*theta3_dot^2 + y1_ddot + d1*sin(theta1)*theta1_ddot + d2*sin(theta2)*theta2_ddot + d3*sin(theta3)*theta3_ddot == y4_ddot;
+
 c7 = x1_ddot == y1_ddot;
+c8 = F_N_x + F_N_y == 0;
 
 %NE:
-n0 = rx1+F == m1*x1_ddot;
-n00 = -ry1 - m1*g == m1*y1_ddot;
+n0 = rx1+F + F_N_x == m1*x1_ddot;
+n00 = -ry1 - m1*g + F_N_y== m1*y1_ddot;
 
 n1 = -rx1+rx2 == m2*x2_ddot;
 n2 = ry1-ry2 -m2*g == m2*y2_ddot;
@@ -36,8 +38,8 @@ vars = [x1_ddot y1_ddot...
     x3_ddot y3_ddot theta2_ddot ...
     x4_ddot y4_ddot theta3_ddot ...
     rx1 ry1 rx2 ...
-    ry2 rx3 ry3 ];
-eqns = [n0 n00 n1 n2 n3 n4 n5 n6 n7 n8 n9 c1 c2 c3 c4 c5 c6 c7];
+    ry2 rx3 ry3 F_N_x F_N_y];
+eqns = [n0 n00 n1 n2 n3 n4 n5 n6 n7 n8 n9 c1 c2 c3 c4 c5 c6 c7 c8];
 [A,b] = equationsToMatrix(eqns,vars)
 
 rank(A)
@@ -75,14 +77,12 @@ X0_5 = [0,0,0]; %x2 y2 theta1 _DOTS
 X0_6 = [0,0,0]; %x3 y3 theta2 _DOTS
 X0_7 = [0,0,0]; %x4 y4 theta3 _DOTS
 
-F = 0;
+F = 60;
 
 X0 = [X0_0, X0_1, X0_2, X0_3, X0_4, X0_5, X0_6, X0_7];
 
 % Create time span for use with ode45
 
-x_ddot_log = [];
-y_ddot_log = [];
 
 t = [0 15];
 % Create function f(t,X) for use with ode45
@@ -123,7 +123,7 @@ x4_dot = X(20);
 y4_dot = X(21);
 theta3_dot = X(22);
 
-%A = DAE_A_funs(D1,D2,D3,IG1,IG2,IG3,M1,M2,M3,M4,THETA1,THETA2,THETA3)
+%A = DAE_A_funs(D1,D2,D3,IG1,IG2,IG3,M1,M2,M3,M4,THETA1,THETA2,THETA3,Y1)
 A = DAE_A_funs(d1,d2,d3,ig1,ig2,ig3,m1,m2,m3,m4,theta1,theta2,theta3);
 %B = DAE_B_funs(F,D1,D2,D3,G,M1,M2,M3,M4,THETA1,THETA2,THETA3,THETA1_DOT,THETA2_DOT,THETA3_DOT)
 b = DAE_B_funs(F,d1,d2,d3,G,m1,m2,m3,m4,theta1,theta2,theta3,theta1_dot,theta2_dot,theta3_dot);
