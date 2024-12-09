20d76bd061619860ee9d79f6ea535ee2dc05c9f0
Mark Lohatepanont <mlohate1@swarthmore.edu>
2024-12-08 23:03:28 -0500

    first_draft_report

--- Plot_EL.m
+++ Plot_EL.m
@@ -92,3 +92,59 @@ gib_sin = z;
 end
 
 
+%% Energy
+% X0_0 = [0, 10, 0,0,0];
+% X0_1 = [0,0,0,0,0]; %xg_dot yg_dot, theta1_dot, theta2_dot, theta3_dot
+% X0 = [X0_0, X0_1];
+xg = X(:,1);
+yg = X(:,2);
+theta1 = X(:,3);
+theta2 = X(:,4);
+theta3 = X(:,5);
+
+xg_dot = X(:,6);
+yg_dot = X(:,7);
+theta1_dot = X(:,8);
+theta2_dot = X(:,9);
+theta3_dot = X(:,10);
+
+m1 = 30;
+m2 = 15;
+m3 = 5;
+m4 = 2;
+
+d1 = 5;
+d2 = 5;
+d3 = 5;
+
+g = 9.81;
+
+Ig1 = 1/12*m2*(2*d1).^2;
+Ig2 = 1/12*m3*(2*d2).^2;
+Ig3 = 1/12*m4*(2*d3).^2;
+
+
+vg = sqrt(xg_dot.^2+yg_dot.^2);
+vg2 = sqrt((xg_dot + d1.*cos(theta1).*theta1_dot).^2 + (yg_dot + d1.*sin(theta1).*theta1_dot).^2);
+vg3 = sqrt((xg_dot + 2.*d1.*cos(theta1).*theta1_dot + d2.*cos(theta2).*theta2_dot).^2 + (yg_dot + 2.*d1.*sin(theta1).*theta1_dot + d2.*sin(theta2).*theta2_dot).^2);
+vg4 = sqrt((xg_dot + 2.*d1.*cos(theta1).*theta1_dot + 2.*d2.*cos(theta2).*theta2_dot + d3.*cos(theta3).*theta3_dot).^2 + (yg_dot + 2.*d1.*sin(theta1).*theta1_dot + 2.*d2.*sin(theta2).*theta2_dot + d3.*sin(theta3).*theta3_dot).^2);
+
+KE = 1/2.* ( m1.*vg.^2 ...
+    +m2.*vg2.^2+Ig1.*theta1_dot.^2 ...
+    +m3.*vg3.^2+Ig2.*theta2_dot.^2 ...
+    +m4.*vg4.^2+Ig3.*theta3_dot.^2 );
+PE = g.* (m1.*yg ...
+    + m2.*(yg-d1.*cos(theta1))...
+    + m3.*(yg-2.*d1.*cos(theta1)-d2.*cos(theta2))...
+    + m4.*(yg -2.*d1.*cos(theta1)-2.*d2.*cos(theta2)-d3.*cos(theta3)));
+% KE = KE-min(KE);
+% PE = PE-min(PE);
+close all
+hold on
+plot(KE,'r','LineWidth',2)
+plot(PE,'g','LineWidth',2)
+plot(KE+PE,'--','color','b','LineWidth',2)
+legend('Kinetic Energy', 'Potential Energy', 'Total Energy','FontSize', 24)
+title('Energy in system','FontSize', 24)
+xlabel('time (s)','FontSize', 24)
+ylabel('Energy (J)','FontSize', 24)
